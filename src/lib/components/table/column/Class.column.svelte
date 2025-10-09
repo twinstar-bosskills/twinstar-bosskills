8e04313b3d574552a0b26fa1b8af3b8277d6a10b
mrceperka <mrceperka@gmail.com>
2025-10-09 17:30:13 +0200

    fix: links

--- src/lib/components/table/column/Class.column.svelte
+++ src/lib/components/table/column/Class.column.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Icon from '$lib/components/Icon.svelte';
+	import Link from '$lib/components/Link.svelte';
 	import { talentSpecToString } from '$lib/model';
 	import { realmToExpansion } from '$lib/realm';
 	import type { BosskillCharacter } from '$lib/server/api/schema';
@@ -7,11 +8,18 @@
 
 	export let realm: string;
 	export let character: BosskillCharacter;
+	export let talentSpecHref: string | undefined = undefined;
 
 	$: talentSpecIconUrl = getTalentSpecIconUrl(realm, character.talent_spec);
 	$: talentSpecString = talentSpecToString(realmToExpansion(realm), character.talent_spec);
 </script>
 
 <Icon src={character.classIconUrl} label={character.classString} />
-<Icon src={talentSpecIconUrl} label={talentSpecString} />
+{#if talentSpecHref}
+	<Link href={talentSpecHref}>
+		<Icon src={talentSpecIconUrl} label={talentSpecString} />
+	</Link>
+{:else}
+	<Icon src={talentSpecIconUrl} label={talentSpecString} />
+{/if}
 <Icon src={character.raceIconUrl} label={character.raceString} />
