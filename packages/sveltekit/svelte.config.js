0c044c52925ac07efbd6da5e9db914ab3ea5ba25
mrceperka <mrceperka@gmail.com>
2025-10-29 22:52:24 +0100

    bun might actually work

--- packages/sveltekit/svelte.config.js
+++ packages/sveltekit/svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-node';
-// import adapter from 'svelte-adapter-bun';
+import nodeAdapter from '@sveltejs/adapter-node';
+import bunAdapter from 'svelte-adapter-bun';
+
 // import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -13,7 +14,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: process.versions.bun ? bunAdapter() : nodeAdapter()
 	}
 };
 
